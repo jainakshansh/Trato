a63ad2c990aa362f1eade70b0d60635ddc2fc242
Tushar Gupta <tush1998@gmail.com>
2017-10-08 00:30:18 +0530

    Functionality Bump 1
    - Added Login Validation
    - Search Page Frame
    - Live JSON Response from Nestoria API
    To do:
    - Manipulate JSON result and pass on to SearchResults Frame for Display
    - Operations on Search Results UI
    - Filters (maybe)

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import tkinter as tk
+import Tkinter as tk
+import requests
 
 class Login:
     def __init__(self, master):
@@ -10,32 +11,83 @@ class Login:
         self.entry1.pack()
         self.label2 = tk.Label(self.frame, text="Password", width=25)
         self.label2.pack()
-        self.entry2 = tk.Entry(self.frame, bd=1)
+        self.entry2 = tk.Entry(self.frame, show='*', bd=1)
         self.entry2.pack()
-        self.button1 = tk.Button(self.frame, text = 'Login', width = 15, command = self.new_window)
+        self.button1 = tk.Button(self.frame, text='Login', width=15, command=self.validation)
         self.button1.pack(pady=10)
         self.frame.pack()
 
-    def new_window(self):
+    def validation(self):
+        user = self.entry1.get()
+        password = self.entry2.get()
+        if user == "tush" and password == "pass":
+            print "Correct Pass"
+            self.loginToSearch()
+
+        else:
+            print "Invalid Pass"
+
+    def loginToSearch(self):
         self.newWindow = tk.Toplevel(self.master)
         self.app = Home(self.newWindow)
 
+    def close_windows(self):
+        self.master.destroy()
 
 class Home:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Sign Out', width = 25, command = self.close_windows)
+        self.label1 = tk.Label(self.frame, text="Search for property (Enter Area / Locality) ", width=50)
+        self.label1.pack()
+        self.searchQuery = tk.Entry(self.frame, bd=1)
+        self.searchQuery.pack()
+        query = self.searchQuery.get()
+        self.SubmitButton = tk.Button(self.frame, text='Search', width=25, command=self.search)
+        self.SubmitButton.pack()
+        self.quitButton = tk.Button(self.frame, text='Quit', width=25, command=self.close_windows)
         self.quitButton.pack()
         self.frame.pack()
 
+    def search(self):
+        print self.searchQuery.get()
+        print "Hello"
+        url = "https://api.nestoria.in/api"
+
+        querystring = {"encoding": "json", "pretty": "1", "action": "search_listings", "country": "in",
+                       "listing_type": "buy", "place_name": self.searchQuery.get()}
+
+        response = requests.request("GET", url, params=querystring)
+        print response.json()
+        result = response.json()
+        print "PRETTY RESULT: "
+        print result["response"]["listings"][0]["title"]
+        self.searchToResults()
+
+    def searchToResults(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = SearchResults(self.newWindow)
+
     def close_windows(self):
         self.master.destroy()
+
+
+class SearchResults:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.title = tk.Label(self.frame, text="Hello", width=50)
+        self.title.pack()
+
+    def close_windows(self):
+        self.master.destroy()
+
 
 def main():
     root = tk.Tk()
     app = Login(root)
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
